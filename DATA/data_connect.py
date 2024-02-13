1f638f82b6c393f85fcf9e7505c4e1d1ae501559
RoFhdsa <151877297+RoFhdsa@users.noreply.github.com>
2024-02-13 22:22:06 +0400

    130224

--- DATA/data_connect.py
+++ DATA/data_connect.py
@@ -17,10 +17,22 @@ class Users:
     password: str = "secret_sauce"
 
 @dataclass
-class pages_authentication:
+class PegaAuthentication:
     """список полей на странице аутентификации"""
     marker: str = "id"
     title_lgn: str = "user-name"
     title_pswrd: str = "password"
     title_enter: str = "login-button"
+    title_enter_err: str = "login_button_container"
+    additional_locator_error: str = '/div/form/div[3]/h3'
 
+@dataclass
+class PegaElements:
+    """локаторы """
+    marker: str = "id"
+    title_inventory: str = "inventory_container"
+    add_title_inventory: str = '/ div'
+    locator_div: dict = field(default_factory= lambda  :
+    {'name':'inventory_item_name',
+                      'price':'inventory_item_price',
+                      'inventory_item_img':'inventory_item_img'})
